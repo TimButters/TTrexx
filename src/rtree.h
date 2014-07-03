189404c382b51d46113d269329ad75fafb5d279a
Tim Butters <tim.butters@sabisu.co>
2014-07-03 16:19:03 +0100

    Implementation must be in header file, but can be outside the class def.

--- src/rtree.h
+++ src/rtree.h
@@ -12,19 +12,25 @@ template <class T> class RTree : public Tree<T> {
     void condense() {}
     void adjust() {}
     
-    void insert(T entry)
-    {
-      this->children.push_back(std::make_tuple(1.0,2.0,3.0, entry));
-    }
-    void print_tree() //For debugging
-    {
-      for (auto &each : this->children) {
-        std::cout << std::get<0>(each) << std::endl;
-        std::cout << std::get<1>(each) << std::endl;
-        std::cout << std::get<2>(each) << std::endl;
-        std::cout << std::get<3>(each) << std::endl;
-      }
-    }
+    void insert(T entry);
+    void print_tree(); //For debugging
 };
+
+template <class T>
+void RTree<T>::insert(T entry)
+{
+  this->children.push_back(std::make_tuple(1.0,2.0,3.0, entry));
+}
+
+template <class T>
+void RTree<T>::print_tree() //For debugging
+{
+  for (auto &each : this->children) {
+    std::cout << std::get<0>(each) << std::endl;
+    std::cout << std::get<1>(each) << std::endl;
+    std::cout << std::get<2>(each) << std::endl;
+    std::cout << std::get<3>(each) << std::endl;
+  }
+}
 
 #endif
